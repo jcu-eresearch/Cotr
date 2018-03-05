8d4ddfc3feb034367eb6d139d66bd6357d9d6e99
Saira Viqar <sairaviqar@gmail.com>
2018-03-05 10:04:55 +1000

    Simplified Dashboard

--- dash.js
+++ dash.js
@@ -1,112 +1,233 @@
-angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
-.config(function($mdThemingProvider) {
-   $mdThemingProvider.theme('altTheme')
-   .primaryPalette('grey',{
-     'default': '900'})
-   .accentPalette('grey',{
-     'default': '500'})
-     // .backgroundPalette('grey',{'default': '900'})
-   .dark()
-   $mdThemingProvider.theme('default')
-   .primaryPalette('grey')
-   .backgroundPalette('grey',{
-      'default': '200'})
-    .accentPalette('green');
-
-  $mdThemingProvider.setDefaultTheme('defuault');
-  $mdThemingProvider.alwaysWatchTheme(true);
-})
-.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log, $timeout,$mdDialog) {
-  var map;
-
-  $scope.showAlert = function(ev) {
-    // Appending dialog to document.body to cover sidenav in docs app
-    // Modal dialogs should fully cover application
-    // to prevent interaction outside of dialog
-    $mdDialog.show(
-      $mdDialog.alert()
-
-        .clickOutsideToClose(true)
-        .title('You are viewing the Classroom on the Reef Dashboard')
-        .textContent('Orpheus Island Research Station')
-        .ariaLabel('James Cook University')
-        .ok('OK!')
-
-    );
-  };
-  $scope.initDashMap=function() {
-
-      map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -18.617, lng: 146.500},
-          mapTypeId: 'hybrid',
-          zoom:12,
-          scrollwheel:  false
-      });
-
-      map.data.setStyle({
-          strokeColor: '#66bb6a',
-          fillColor: '#66bb6a',
-          fillOpacity: 0.1,
-          strokeWeight: 2
-      });
-
-
-      var marker1 = new google.maps.Marker({
-          position: {lat: -18.617, lng: 146.500},
-          map: map
-      });
-
-      var markers=[marker1];
 
 
 
-      function definePopup(id, source1, source2) {
-          var popupText = '<video id="my-video" class="video-js" controls preload="auto" width="640" height="264"\n' +
-              '           data-setup="{}">\n' +
-              '<source src="'+source1+'" type=\'video/mp4\'>\n' +
-              '\n' +
-              '<p class="vjs-no-js">\n' +
-              'To view this video please enable JavaScript, and consider upgrading to a web browser that\n' +
-              '<a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>\n' +
-              '</p>\n' +
-              '</video>';
-          return popupText;
-      }
+initMap=function() {
 
-      var infowindow = new google.maps.InfoWindow({
-          content: definePopup('http://marfu-cam-fishtank.jcu.edu.au/mjpg/1/video.mjpg?camera=1')
-      });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat:-18.612105, lng: 146.49343},
+        mapTypeId: 'hybrid',
+        zoom: 16,
+        scrollwheel:  false
+    });
 
-      marker1.addListener('click', function() {
-          infowindow.open(map, marker1);
-      });
+    map.data.setStyle({
+        strokeColor: '#66bb6a',
+        fillColor: '#66bb6a',
+        fillOpacity: 0.1,
+        strokeWeight: 2
+    });
 
 
 
-  };
+    var contentString1 = '<div class="popup-content">'+
+        //'<h6 id="firstHeading" class="firstHeading">Sensor Platform</h6>'+
+        //'<div class="row flex-items-start">'+
+        '<div class="popup-img "><img  src="images/platform1.jpg" alt="Sensor Platform" width="100%" ></div>'+
+        //'<div class="col-xs-6" class="popup-text"><p ><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large</p></div>'+
+        '</div> ';
 
-  $timeout(function(){$scope.initDashMap();});
+    var infowindow1 = new google.maps.InfoWindow({
+        content: contentString1
+    });
 
-})
-.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function () {
-    $mdSidenav('left').close()
-      .then(function () {
-        $log.debug("close LEFT is done");
-      });
-  };
-})
-.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function () {
-    $mdSidenav('right').close()
-      .then(function () {
-        $log.debug("close RIGHT is done");
-      });
-  };
-})
 
-.controller('PlotlyCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.temperature=[{
+    var marker1 = new google.maps.Marker({
+        position: {lat: -18.61180718753694, lng: 146.4860380712551},
+        map: map,
+        title: "West Sensor Platform"
+    });
+
+    var marker11 = new google.maps.Marker({
+        position: {lat: -18.61180718753694, lng: 146.4860380712551},
+        map: map,
+        label: {
+            text: "West Sensor Platform ",
+            color: "white"
+        },
+        title: "West Sensor Platform",
+        icon: {
+            labelOrigin: new google.maps.Point(11, 50),
+            url: ' ',
+            size: new google.maps.Size(22, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(11, 40),
+        }
+    });
+
+    marker11.addListener('click', function() {
+        infowindow1.open(map, marker11);
+    });
+
+
+
+    var contentString2 = '<div class="popup-content">'+
+        //'<h6 id="firstHeading" class="firstHeading">Sensor Platform</h6>'+
+        //'<div class="row flex-items-start">'+
+        '<div class="popup-img "><img  src="images/platform2.jpg" alt="Sensor Platform" width="100%" ></div>'+
+        //'<div class="col-xs-6" class="popup-text"><p ><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large</p></div>'+
+        '</div> ';
+
+    var infowindow2 = new google.maps.InfoWindow({
+        content: contentString2
+    });
+
+
+    var marker2 = new google.maps.Marker({
+        position: {lat: -18.6123192163581, lng: 146.5002565912303},
+        map: map,
+        title: "East Sensor Platform"
+    });
+
+    var marker22 = new google.maps.Marker({
+        position: {lat: -18.6123192163581, lng: 146.5002565912303},
+        map: map,
+        label: {
+            text: "East Sensor Platform",
+            color: "white"
+        },
+        title: "East Sensor Platform",
+        icon: {
+            labelOrigin: new google.maps.Point(11, 50),
+            url: ' ',
+            size: new google.maps.Size(22, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(11, 40),
+        }
+    });
+
+    marker22.addListener('click', function() {
+        infowindow2.open(map, marker22);
+    });
+
+
+
+    var contentString3 = '<div class="popup-content">'+
+        //'<h6 id="firstHeading" class="firstHeading">Sensor Platform</h6>'+
+        //'<div class="row flex-items-start">'+
+        '<div class="popup-img "><img  src="images/mangroves.jpg" alt="Sensor Platform" width="100%" ></div>'+
+        //'<div class="col-xs-6" class="popup-text"><p ><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large</p></div>'+
+        '</div> ';
+
+    var infowindow3 = new google.maps.InfoWindow({
+        content: contentString3
+    });
+
+
+
+    var marker33 = new google.maps.Marker({
+        position: {lat: -18.61213598839285, lng: 146.4889281705785},
+        map: map,
+        label: {
+            text: "Mangroves",
+            color: "white"
+        },
+        title: "Mangroves",
+        icon: {
+            labelOrigin: new google.maps.Point(18, 42),
+            url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+            size: new google.maps.Size(25, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(11, 40),
+        }
+    });
+
+    marker33.addListener('click', function() {
+        infowindow3.open(map, marker33);
+    });
+
+
+
+    var contentString4 = '<div class="popup-content">'+
+        //'<h6 id="firstHeading" class="firstHeading">Sensor Platform</h6>'+
+        //'<div class="row flex-items-start">'+
+        '<div class="popup-img "><img  src="images/accomodation.jpg" alt="Sensor Platform" width="100%" ></div>'+
+        //'<div class="col-xs-6" class="popup-text"><p ><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large</p></div>'+
+        '</div> ';
+
+    var infowindow4 = new google.maps.InfoWindow({
+        content: contentString4
+    });
+
+    var marker44 = new google.maps.Marker({
+        position: {lat: -18.61413911560015, lng: 146.4896363148613},
+        map: map,
+        label: {
+            text: "Accomodation",
+            color: "white"
+        },
+        title: "Accomodation",
+        icon: {
+            labelOrigin: new google.maps.Point(18, 42),
+            url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+            size: new google.maps.Size(25, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(11, 40),
+        }
+    });
+
+    marker44.addListener('click', function() {
+        infowindow4.open(map, marker44);
+    });
+
+
+    var contentString5 = '<div class="popup-content">'+
+        //'<h6 id="firstHeading" class="firstHeading">Sensor Platform</h6>'+
+        //'<div class="row flex-items-start">'+
+        '<div class="popup-img "><img  src="images/laboratories.jpg" alt="Sensor Platform" width="100%" ></div>'+
+        //'<div class="col-xs-6" class="popup-text"><p ><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large</p></div>'+
+        '</div> ';
+
+    var infowindow5 = new google.maps.InfoWindow({
+        content: contentString5
+    });
+
+    var marker55 = new google.maps.Marker({
+        position: {lat: -18.61342522001228, lng: 146.4902400333704},
+        map: map,
+        label: {
+            text: "Labs",
+            color: "white"
+        },
+        title: "Laboratories",
+        icon: {
+            labelOrigin: new google.maps.Point(18, 42),
+            url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+            size: new google.maps.Size(25, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(11, 40),
+        }
+    });
+
+    marker55.addListener('click', function() {
+        infowindow5.open(map, marker55);
+    });
+
+    var markers=[ marker33, marker44, marker55];
+
+    var markers2=[ marker11, marker22];
+
+    map.addListener( 'zoom_changed', function() {
+        var zoom = map.getZoom();
+
+        // iterate over markers and call setVisible
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setVisible(zoom >= 15);
+        }
+
+        for (var i = 0; i < markers2.length; i++) {
+            markers2[i].setVisible(zoom >= 14);
+        }
+
+
+    });
+
+
+};
+
+
+
+makePlots =function(){
+  var temperature=[{
     x: [],
     y: [],
     name: 'Temperature',
@@ -115,7 +236,7 @@ angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
     }
   }];
 
-  $scope.layout = {
+  var layout = {
     yaxis:{
         title: 'Temperature (C)',
         color: "white",
@@ -127,7 +248,7 @@ angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
 
   //plotly stuff
 
-  $scope.makeplot= function() {
+  makeplot= function() {
     Plotly.d3.csv("data/sample.csv", function(data){ processData(data) } );
 
     function processData(allRows) {
@@ -170,29 +291,24 @@ angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
       }];
 
       var layout1 = {
+        title: 'Temperature',
         yaxis:{
             title: 'Temperature (C)',
-            autorange: true//,
-            //color: "white"
-        },
-        xaxis: {
-          //color: "white"
+            autorange: true
         },
         legend: {
           x: 1.05,
           y: 1
         },
-        //plot_bgcolor :'#424242',
-        //paper_bgcolor: '#424242',
         margin: {
                     l: 100,
                     r: 50,
                     b: 50,
-                    t: 0,
                     pad: 0
                 }
       };
       var layout2 = {
+        title: 'Transmission',
         yaxis:{
             title: 'Transmission',
             autorange: true//,
@@ -205,36 +321,28 @@ angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
           x: 1.05,
           y: 1
         },
-        //plot_bgcolor :'#424242',
-        //paper_bgcolor: '#424242',
         margin: {
                     l: 100,
                     r: 50,
                     b: 50,
-                    t: 0,
                     pad: 0
                 }
       };
       var layout3 = {
+        title: 'Direction',
         yaxis:{
             title: 'Direction (Rad CCW from East)',
             autorange: true//,
-            //color: "white"
-        },
-        xaxis: {
-          //color: "white"
+
         },
         legend: {
           x: 1.05,
           y: 1
         },
-        //plot_bgcolor :'#424242',
-        //paper_bgcolor: '#424242',
         margin: {
                     l: 100,
                     r: 50,
                     b: 50,
-                    t: 0,
                     pad: 0
                 }
       };
@@ -244,7 +352,7 @@ angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
 
 
     }
-  }
+  };
 
   var d3 = Plotly.d3;
   var temp = d3.select("div[id='temperature']");
@@ -265,22 +373,8 @@ angular.module('CotrApp',['ngMaterial', 'ngMessages' ])
     Plotly.Plots.resize(dir);
   }
 
-  $scope.makeplot();
+  makeplot();
 
-})
+}
 
-
-.directive('picSlider', function () {
-    return {
-        templateUrl: 'templates/slider.html',
-        controller: function ($timeout){
-
-            $timeout( function () {
-                $(document).ready(function () {
-                    $('.carousel.carousel-slider').carousel({fullWidth: true});
-                });
-            });
-
-        }
-    };
-});
+makePlots();
